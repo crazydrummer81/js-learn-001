0227432b339a2c90dd2517e2e0a8ebf9a63aee27
crazydrummer81 <crazy_drummer@mail.ru>
2020-08-29 22:19:58 +0600

    Выполнено ДЗ 1

---
+++ httpdocs/dz-001/js/test.js
@@ -0,0 +1,13 @@
+'use strict';
+const numberOfFilms = 2;
+let personalMovieDB = {
+	'count' : numberOfFilms,
+	'movies' : {},
+	'actors' : {},
+	'genres' : [],
+	'privat' : false,
+};
+
+let a = {'qwe': 2, 'asd': 5};
+a['zxc'] = 5;
+console.dir(a);
