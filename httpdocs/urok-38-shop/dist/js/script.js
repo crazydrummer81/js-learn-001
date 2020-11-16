939b50586c8aba2b4c626d33828098a45f6e7328
crazydrummer81 <crazy_drummer@mail.ru>
2020-11-16 12:49:50 +0600

    Lesson 53 done

--- httpdocs/urok-38-shop/dist/js/script.js
+++ httpdocs/urok-38-shop/dist/js/script.js
@@ -311,6 +311,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ;
   window.addEventListener('scroll', showModalByScroll);
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо',
+    fail: 'Что-то пошло не так...'
+  };
+  forms.forEach(form => {
+    postData(form);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      request.setRequestHeader('Content-type', 'application/json');
+      const formData = new FormData(form);
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      const json = JSON.stringify(object);
+      request.send(json);
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => statusMessage.remove(), 2000);
+        } else {
+          statusMessage.textContent = message.fail;
+        }
+      });
+    });
+  }
 });
 
 function getZero(num) {
